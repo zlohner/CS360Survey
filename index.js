08e8ca74b876ba966d13e609abc13e7e9a60bdbb
Sicilica <schixxism@gmail.com>
2015-12-01 16:59:28 -0700

    rename app -> client

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var express = require('express')
 
 
 var app = express()
-app.use(express.static('app'))
+app.use(express.static('client'))
 app.listen(80, function() {
 	// we have a callback for some reason?
+})
+
+app.get('/cat', function(req, res) {
+	res.send('it\'s a cat!')
 })
