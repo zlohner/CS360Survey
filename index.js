9ee01b56558f28160c985a1d2a36e4e8476d3b5b
Sicilica <schixxism@gmail.com>
2015-12-02 21:09:13 -0700

    add loader for API

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 var express = require('express')
+var fs = require('fs')
 
 
+// start the server
 var app = express()
 app.use(express.static('client'))
 app.listen(80, function() {
-	// we have a callback for some reason?
+	console.log('Listening on port 80.')
 })
 
-app.get('/cat', function(req, res) {
-	res.send('it\'s a cat!')
-})
+
+// load all api endpoints
+function apiLoad(folder) {
+	app.get('/api'+folder, function(req, res) {
+		res.status(404).send('Cannot GET /api'+folder)
+	})
+
+	fs.readdir('server'+folder, function(err, files) {
+		files.forEach(function(file) {
+			fs.stat('server'+folder+file, function(err, stats) {
+				if (stats.isDirectory())
+					apiLoad(folder+file+'/')
+				else if (file.slice(-3) == '.js')
+					app.get('/api'+folder+file.slice(0, -3), require('./server'+folder+file))
+			})
+		})
+	})
+}
+apiLoad('/')
